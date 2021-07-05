13873eac7ea7e200748e15fb6e77e7c12e7e5296
nemscep <nemscepanovic@gmail.com>
2021-07-05 20:58:33 +0200

    Move MainActivity.kt to ui package

--- app/src/main/java/com/nemscep/muffin/ui/MainActivity.kt
+++ app/src/main/java/com/nemscep/muffin/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.nemscep.muffin
+package com.nemscep.muffin.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nemscep.muffin.R.layout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
     }
 }
