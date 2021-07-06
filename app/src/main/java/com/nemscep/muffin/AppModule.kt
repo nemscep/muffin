bb335eb971005f2cd55f6d1497051a1ca49b06c8
nemscep <nemscepanovic@gmail.com>
2021-07-06 22:32:36 +0200

    Init koin

---
+++ app/src/main/java/com/nemscep/muffin/AppModule.kt
@@ -0,0 +1,7 @@
+package com.nemscep.muffin
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
---
+++ app/src/main/java/com/nemscep/muffin/MuffinApplication.kt
@@ -0,0 +1,23 @@
+package com.nemscep.muffin
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.module.Module
+
+class MuffinApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin{
+            androidLogger()
+            androidContext(this@MuffinApplication)
+            modules(modules)
+        }
+    }
+}
+
+val modules = listOf<Module>(
+    appModule
+)
