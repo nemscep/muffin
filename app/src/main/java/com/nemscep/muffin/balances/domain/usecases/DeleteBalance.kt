bbd368606397b003d74613c03bad22aa6b619b48
nemscep <nemscepanovic@gmail.com>
2021-07-12 20:57:07 +0200

    Introduce DeleteBalance use case

---
+++ app/src/main/java/com/nemscep/muffin/balances/domain/usecases/DeleteBalance.kt
@@ -0,0 +1,14 @@
+package com.nemscep.muffin.balances.domain.usecases
+
+import com.nemscep.burrito.CompositeFailure
+import com.nemscep.burrito.Outcome
+import com.nemscep.muffin.balances.domain.entities.Balance
+import com.nemscep.muffin.balances.domain.repo.BalancesRepository
+
+/**
+ * Use case responsible for deletion balance set by user.
+ */
+class DeleteBalance(private val balancesRepository: BalancesRepository) {
+    suspend operator fun invoke(balance: Balance): Outcome<Unit, CompositeFailure<Nothing>> =
+        balancesRepository.deleteBalance(balance = balance)
+}
---
+++ app/src/test/java/com/nemscep/muffin/balances/domain/usecases/DeleteBalanceTest.kt
@@ -0,0 +1,53 @@
+package com.nemscep.muffin.balances.domain.usecases
+
+import com.nemscep.burrito.CompositeFailure.Network.Unspecified
+import com.nemscep.burrito.Outcome.Failure
+import com.nemscep.burrito.Outcome.Success
+import com.nemscep.muffin.balances.domain.entities.Balance.MainBalance
+import com.nemscep.muffin.balances.domain.repo.BalancesRepository
+import com.nemscep.muffin.profile.domain.entities.Currency.EUR
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runBlockingTest
+import org.amshove.kluent.shouldEqual
+import org.junit.Test
+
+
+class DeleteBalanceTest {
+    private val balancesRepository: BalancesRepository = mockk(relaxed = true)
+    private lateinit var tested: DeleteBalance
+
+    @Test
+    fun `when deleting balance is successful, success is returned`() = runBlockingTest {
+        // Given
+        val expected = Success(Unit)
+        coEvery { balancesRepository.deleteBalance(MAIN_BALANCE) } returns expected
+        `given tested use case`()
+
+        // When
+        val outcome = tested(MAIN_BALANCE)
+
+        // Then
+        outcome shouldEqual expected
+    }
+
+    @Test
+    fun `when deleting balance is unsuccessful, failure is returned`() = runBlockingTest {
+        // Given
+        val expected = Failure(Unspecified(error = Exception()))
+        coEvery { balancesRepository.deleteBalance(MAIN_BALANCE) } returns expected
+        `given tested use case`()
+
+        // When
+        val outcome = tested(MAIN_BALANCE)
+
+        // Then
+        outcome shouldEqual expected
+    }
+
+    private fun `given tested use case`() {
+        tested = DeleteBalance(balancesRepository = balancesRepository)
+    }
+}
+
+private val MAIN_BALANCE = MainBalance(value = 1000, currency = EUR)
